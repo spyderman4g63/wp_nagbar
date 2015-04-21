6ed5cdaadef66a2b4b22acbc178ed58141dd0f78
John Ward <spyderman4g63@gmail.com>
2015-04-20 20:26:11 -0400

    admin page

--- admin.php
+++ admin.php
@@ -1,15 +1,7 @@
 <?php
 // admin page settings
 
-add_action( 'admin_init', 'nagbar_settings' );
-function nagbar_settings() {
-  register_setting( 'nagbar-settings-group', 'content' );
-}
-
-add_action( 'admin_menu', 'nagbar_menu_page' );
-
-function nagbar_menu_page() {
-
-	add_menu_page( 'nagbar', 'custom menu', 'manage_options', 'myplugin/myplugin-admin.php', '', plugins_url( 'myplugin/images/icon.png' ), 6 );
-
-}
+?>
+<div class="wrap">
+  <h2>Nagbar Settings</h2>
+</div>
